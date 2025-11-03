059d1cffc8fbe9848362a22bf8a4d08eab209f60
Nhu Khanh <21522204@gm.uit.edu.vn>
2025-11-03 19:17:11 +0700

    activate rate limiter

--- backend/src/api/controller/userController/changeEmailController.ts
+++ backend/src/api/controller/userController/changeEmailController.ts
@@ -4,21 +4,21 @@ import catchAsync from "../../utils/catchAsync";
 import { createLimiter } from "../../utils/createLimiter";
 
 // --------------------------- limiters ---------------------------
-// export const changeEmailLimiterByUser = createLimiter({
-//   max: 1,
-//   windowMs: 3 * 60 * 1000,
-//   message:
-//     "You can only request a password reset once every 3 minutes with your account.",
-//   keyGenerator: (req) => req.user?.id,
-// });
+export const changeEmailLimiterByUser = createLimiter({
+  max: 1,
+  windowMs: 3 * 60 * 1000,
+  message:
+    "You can only request a password reset once every 3 minutes with your account.",
+  keyGenerator: (req) => req.user?.id,
+});
 
-// export const changeEmailLimiterByIP = createLimiter({
-//   max: 10,
-//   windowMs: 60 * 60 * 1000,
-//   message:
-//     "You can only request a password reset 10 times every 1 hour with your device.",
-//   keyGenerator: (req) => req.ip || "",
-// });
+export const changeEmailLimiterByIP = createLimiter({
+  max: 10,
+  windowMs: 60 * 60 * 1000,
+  message:
+    "You can only request a password reset 10 times every 1 hour with your device.",
+  keyGenerator: (req) => req.ip || "",
+});
 // --------------------------- limiters ---------------------------
 
 // --------------------------- controllers ---------------------------
