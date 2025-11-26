4c4cbd76e01a2785a06ba6fb30da10ef2a40b308
Nhu Khanh <21522204@gm.uit.edu.vn>
2025-11-26 23:54:44 +0700

    test call api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
 /** @format */
 
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
-  return <>TEST FRONT END</>;
+  const [blog, setBlog] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get("/api/v1/blogs");
+
+      setBlog(response.data);
+    }
+    fetchData();
+  }, []);
+
+  return <>{blog}</>;
 }
 
 export default App;
