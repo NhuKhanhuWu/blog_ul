506875181ced98400b0709051fd9f1ce2d384ea7
Nhu Khanh <21522204@gm.uit.edu.vn>
2026-03-23 15:22:00 +0700

    update: use zod to validate toggle vote

--- backend/src/api/router/voteRouter.ts
+++ backend/src/api/router/voteRouter.ts
@@ -3,10 +3,14 @@
 import express from "express";
 import { protect } from "../controller/authController/protectController";
 import { toggleVote } from "../controller/voteController/toggleVoteController";
+import { validate } from "../utils/validate";
+import { toggleVoteSchema } from "../utils/schema/voteSchema";
 
 const voteRouter = express.Router();
 
 // toggle vote
-voteRouter.route("/").post(protect, toggleVote);
+voteRouter
+  .route("/")
+  .post(protect, validate(toggleVoteSchema, "body"), toggleVote);
 
 export default voteRouter;
