dc26394f1e4e3eee6909fb803a70cc726a0be541
Nhu Khanh <21522204@gm.uit.edu.vn>
2026-06-27 16:41:56 +0700

    refactor: move auth routes to auth.router.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,6 +14,7 @@ import cmtRouter from "./api/routers/comment.router";
 import voteRouter from "./api/routers/vote.router";
 import categoryRouter from "./api/routers/category.router";
 import { globalLimiter } from "./api/middlewares/global.middleware";
+import authRouter from "./api/routers/auth.router";
 
 const app = express();
 app.use(express.json());
@@ -42,6 +43,7 @@ app.use(globalLimiter);
 
 // ROUTER
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/blogs", blogRouter);
 app.use("/api/v1/blog-list", blogListRouter);
 app.use("/api/v1/cmt", cmtRouter);
