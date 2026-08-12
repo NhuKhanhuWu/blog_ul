236b4c7600a7c65426976a72cb200e5f36aa6f93
Nhu Khanh <21522204@gm.uit.edu.vn>
2026-08-12 15:22:59 +0700

    add supabase upload img helper

--- backend/src/api/supabase/supabase.ts
+++ backend/src/api/supabase/supabase.ts
@@ -4,6 +4,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.SUPABASE_URL || "";
 const supabaseKey = process.env.SUPABASE_KEY || "";
+
 const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
     autoRefreshToken: false,
